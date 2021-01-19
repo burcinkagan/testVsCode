5d5b97991c111d120b60a97b1be1642858be4240
Burçin Kağan <77413560+burcinkagan@users.noreply.github.com>
2021-01-19 19:08:02 +0300

    test

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+function Test() {
+    return (
+      <div> 
+        <ol>
+          <li>aaa</li>
+          <li>vvv</li>
+          <li>ccc</li>
+        </ol>
+      </div>
+    );
+  }
+
+  export default Test;
